69159a096fdad24910a67142bcf75b8539306b07
James Yin <jamesyinbaare@gmail.com>
2026-01-07 23:08:44 +0000

    Format files

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-import uvicorn
-from fastapi import FastAPI, status
 from typing import Any
 
+import uvicorn
+from fastapi import FastAPI, status
+
 app = FastAPI(title="My Skin Scanner")
+
 
 @app.get("/", status_code=status.HTTP_200_OK)
 def test() -> dict[str, Any]:
